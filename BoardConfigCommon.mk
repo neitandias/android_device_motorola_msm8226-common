97735141589e599ef8beead167c6fb009c801bac
RenanQueiroz <queirozrrq@gmail.com>
2019-03-31 15:27:08 -0400

    msm8226-common: Never perform a full dexpreopt

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -159,15 +159,13 @@ SF_START_GRAPHICS_ALLOCATOR_SERVICE := true
 # Basic dexpreopt
 ifeq ($(HOST_OS),linux)
   ifneq ($(TARGET_BUILD_VARIANT),eng)
-    ifeq ($(WITH_DEXPREOPT),)
-      WITH_DEXPREOPT := true
-    endif
+    WITH_DEXPREOPT_DEBUG_INFO := false
+    USE_DEX2OAT_DEBUG := false
+    DONT_DEXPREOPT_PREBUILTS := true
+    WITH_DEXPREOPT := false
+    WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
   endif
 endif
-WITH_DEXPREOPT_DEBUG_INFO := false
-USE_DEX2OAT_DEBUG := false
-DONT_DEXPREOPT_PREBUILTS := true
-WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
 
 # SELinux
 include device/qcom/sepolicy-legacy/sepolicy.mk
